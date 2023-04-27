eef6825b5d3aa9dad609df8629483e3de12ffcba
Aahvaanit <tripathi.aahvaanit@gmail.com>
2023-04-27 19:52:57 +0530

    mindblowing progress

--- my_project/project.py
+++ my_project/project.py
@@ -1,3 +1,34 @@
 import cv2
 
-print("hello")
+# pre-trained face detection
+trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# taking image
+img = cv2.imread("RDJ.jpg")
+
+# converts img to grayscale
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# detects faces
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+# shows image
+cv2.imshow("Face Detector", grayscaled_img)
+cv2.waitKey()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
