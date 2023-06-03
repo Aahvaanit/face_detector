5cef3456472228041bfbd95c46113fc3b491e7c3
Aahvaanit <tripathi.aahvaanit@gmail.com>
2023-06-03 17:44:44 +0530

    smile_detection_integrated-face_detector_modified

--- my_project/smile_detector.py
+++ my_project/smile_detector.py
@@ -15,22 +15,35 @@ while True:
     # reads current frame
     successful_frame_read, frame = webcam.read()
 
+    # abort if error
     if not successful_frame_read:
         break
 
     # converts image to grayscale
-    grayscaled_img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayscaled_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # detects faces 
-    face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+    faces = trained_face_data.detectMultiScale(grayscaled_frame, 1.3, 5)
 
-    # draws rectangle around face(s) in a frame
-    for i in range(len(face_coordinates)):
-        (x, y, w, h) = face_coordinates[i]
+    for (x, y, w, h) in faces:
+        
+        # draws rectangle around face
         cv2.rectangle(frame, (x, y), (x+w, y+h), (79, 252, 5), 2)
 
+        # creates the face sub-image
+        face = frame[y:y+h, x:x+w]
+
+        # changes face to grayscale
+        face_grayscale = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+        smiles = smile_detector.detectMultiScale(face_grayscale, scaleFactor = 1.7, minNeighbors = 20)
+
+        # person is smiling if len(smile) > 0
+        if len(smiles) > 0:
+            cv2.putText(frame, 'smiling', (x, y+h+40), fontScale=3, fontFace = cv2.FONT_HERSHEY_PLAIN, color = (255, 255, 255))
+
     # shows each frame
-    cv2.imshow("FACE DETECTOR", frame)
+    cv2.imshow("SMILE DETECTOR", frame)
 
     # waits for key press   
     key = cv2.waitKey(1)
@@ -42,10 +55,3 @@ while True:
 # stops webcam
 webcam.release()
 cv2.destroyAllWindows()
-
-
-
-
-
-    
-
